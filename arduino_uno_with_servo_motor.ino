72b6006cde4393c7ab631e0a2e6be88a5830c3d7
Engr Muhammad Abid Hussain <110962202+Abid-Maharvi@users.noreply.github.com>
2022-08-23 20:27:57 +0500

    Arduio UNO with Servo Motor Code

---
+++ arduino_uno_with_servo_motor.ino
@@ -0,0 +1,25 @@
+#include<Servo.h>
+Servo Myservo;
+int pos;
+void setup()
+{
+Myservo.attach(3);
+}
+
+void loop()
+{
+  
+  
+for(pos=0;pos<=180;pos++){
+Myservo.write(pos);
+delay(15);
+}
+  delay(1000);
+  
+  for(pos=180;pos>=0;pos--){
+Myservo.write(pos);
+delay(15);
+}
+  delay(1000);
+  
+}
